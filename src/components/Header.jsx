8a7c356a24f32096bfc4e40f9e48e1946a54e1ac
a1shuvo <a1shuvosaha@gmail.com>
2025-04-30 22:23:47 +0600

    Daisy UI theme Customization, Font & Date-FNS Integration

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from '../assets/logo.png'
+import { format } from 'date-fns';
 
 const Header = () => {
     return (
         <div className='flex justify-center flex-col items-center'>
             <img className='w-[350px]' src={logo} alt="logo" />
-            <p>Journalism Without Fear or Favour</p>
+            <p className='text-accent'>Journalism Without Fear or Favour</p>
+            <p className='text-accent'><span className='text-primary'>{format(new Date(), "EEEE")}</span>{format(new Date(), ", MMMM MM, yyyy")}</p>
         </div>
     );
 };
