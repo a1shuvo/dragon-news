201636422def919bcc454cf3cc27477c07bf82f5
a1shuvo <a1shuvosaha@gmail.com>
2025-05-01 10:02:19 +0600

    Design Grid Layout and Fetch Categories

---
+++ src/components/Categories.jsx
@@ -0,0 +1,12 @@
+import React, { use } from 'react';
+const categoryPromise = fetch('/categories.json').then(res => res.json());
+const Categories = () => {
+    const categories = use(categoryPromise);
+    return (
+        <div className='font-bold'>
+            All category ({categories.length})
+        </div>
+    );
+};
+
+export default Categories;
