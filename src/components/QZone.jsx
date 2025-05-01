f9c954e80aab0e9e54ad1e93ac88d904d09ce962
a1shuvo <a1shuvosaha@gmail.com>
2025-05-01 20:27:04 +0600

    Design Right Side QZone component and sticky aside

--- src/components/QZone.jsx
+++ src/components/QZone.jsx
@@ -2,17 +2,22 @@ import React from 'react';
 import swimImg from '../assets/swimming.png'
 import classImg from '../assets/class.png'
 import playImg from '../assets/playground.png'
+import bgImg from '../assets/bg.png'
 
 const QZone = () => {
     return (
-        <div className='bg-base-200 p-3'>
-            <h2 className='font-bold mb-5'>QZone</h2>
-            <div className='space-y-5'>
-                <img src={swimImg} alt="Swiming" />
-                <img src={classImg} alt="Class" />
-                <img src={playImg} alt="Playground" />
+        <>
+            <div className='bg-base-200 p-3'>
+                <h2 className='font-bold mb-5'>QZone</h2>
+                <div className='space-y-5'>
+                    <img src={swimImg} alt="Swimming" />
+                    <img src={classImg} alt="Class" />
+                    <img src={playImg} alt="Playground" />
+                </div>
             </div>
-        </div>
+            <img className='w-full' src={bgImg} alt="Background" />
+        </>
+
     );
 };
 
