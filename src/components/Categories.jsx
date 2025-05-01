1aa241c0f4751f5d4d130ca2bc11f693f6537053
a1shuvo <a1shuvosaha@gmail.com>
2025-05-01 10:43:44 +0600

    Display Categories, Show Params & Social Login options

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,10 +1,24 @@
 import React, { use } from 'react';
+import { NavLink } from 'react-router';
 const categoryPromise = fetch('/categories.json').then(res => res.json());
 const Categories = () => {
     const categories = use(categoryPromise);
     return (
-        <div className='font-bold'>
-            All category ({categories.length})
+        <div>
+            <h2 className='font-bold'>All category</h2>
+            <div className='flex flex-col gap-3 mt-5'>
+                {
+                    categories.map((category) => (
+                        <NavLink
+                            className={"bg-base-100 hover:bg-base-200 border-0 font-semibold text-accent text-left py-2 px-10"}
+                            to={`/category/${category.id}`}
+                            key={category.id}
+                        >
+                            {category.name}
+                        </NavLink>
+                    ))
+                }
+            </div>
         </div>
     );
 };
